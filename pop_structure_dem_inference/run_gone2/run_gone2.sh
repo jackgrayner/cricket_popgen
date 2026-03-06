e80c96428bd5b961cc11c45fd6bcc6d7e828621e
jackgrayner <105981282+jackgrayner@users.noreply.github.com>
2026-03-06 08:30:55 -0500

    Update run_gone2.sh

--- pop_structure_dem_inference/run_gone2/run_gone2.sh
+++ pop_structure_dem_inference/run_gone2/run_gone2.sh
@@ -28,7 +28,7 @@ done
 pop=$1
 
 #create pop bed file 
-plink --bfile all_filtered_chr5_9_14 --chr scaffold_5 scaffold_9 scaffold_14 --thin-count 100000 --recode --keep ${pop} --allow-extra-chr --out ${pop}_filtered_chr5_9_14
+plink --bfile all_filtered_chr5_9_14 --chr scaffold_5 scaffold_9 scaffold_14 --thin-count 1000000 --recode --keep ${pop} --allow-extra-chr --out ${pop}_filtered_chr5_9_14
 
 #then, the full analysis (1M SNPs)
 ~/scratch/popgen/gone2/feb26/GONE2/gone2 -s 1000000 -g 0 -r 1.5 -o ./results_all/${pop}_r1.5_full -t 8 ${pop}_filtered_chr5_9_14.ped
